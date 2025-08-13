10e8d9acf43d3ef2d32e8c1352f2e3ca40513b32
maryam212003 <maryam.s01382@gmail.com>
2025-08-13 20:44:28 +0330

    add server.js

--- storetrack-backend/src/app.js
+++ storetrack-backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import prisma from './prisma/prismaClient.js'; 
 import './jobs/lowStockJob.js'; 
 import productRoutes from './routes/productRoutes.js';
@@ -8,9 +9,9 @@ import orderRoutes from './routes/orderRoutes.js';
 import orderItemRoutes from './routes/orderItemRoutes.js';
 
 const app = express();
-app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('StoreTrack API is running');
@@ -23,8 +24,4 @@ app.use('/stockHistory', stockHistoryRoutes);
 app.use('/orders', orderRoutes);
 app.use('/order-items', orderItemRoutes);
 
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
