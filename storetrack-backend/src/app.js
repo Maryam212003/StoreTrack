a9ca6d275a1d58a3612061539ad885d3e6ba5e86
maryam212003 <maryam.s01382@gmail.com>
2025-08-15 22:33:19 +0330

    Docker v1

--- storetrack-backend/src/app.js
+++ storetrack-backend/src/app.js
@@ -2,13 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import prisma from './prisma/prismaClient.js'; 
 import './jobs/lowStockJob.js'; 
+
+// Routes
 import productRoutes from './routes/productRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import stockHistoryRoutes from './routes/stockHistoryRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import orderItemRoutes from './routes/orderItemRoutes.js';
 import reportRoutes from './routes/reportRoutes.js';
-
 
 const app = express();
 
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('StoreTrack API is running');
 });
 
-// Routes
+// API Routes
 app.use('/products', productRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/stockHistory', stockHistoryRoutes);
@@ -27,5 +28,11 @@ app.use('/orders', orderRoutes);
 app.use('/order-items', orderItemRoutes);
 app.use('/reports', reportRoutes);
 
+// Graceful shutdown (important for Docker)
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM received: closing HTTP server');
+  await prisma.$disconnect();
+  process.exit(0);
+});
 
 export default app;
